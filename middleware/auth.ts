1adfb964590bed73469fc19796cf825dad1b6a19
Anand <anand.e145@gmail.com>
2024-05-28 18:11:55 +0800

    login zasvar

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -2,10 +2,11 @@ import { NextResponse } from 'next/server';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { NextRequest } from 'next/server';
 import { verifyToken } from '@/utils/auth';
-
+export { default } from "next-auth/middleware"
 export async  function middleware(request: NextRequest ) {
   const url = request.nextUrl.clone();
   const authToken = request.cookies.get('authToken');
+  console.log("run");
   if (!authToken) {
     url.pathname = '/auth/login';
     return NextResponse.redirect(url);
@@ -20,8 +21,8 @@ export async  function middleware(request: NextRequest ) {
 }
 
 export const config = {
-  matcher: [ '/'], // protected routes
+//   matcher: [ '/'], // protected routes
+    matcher: '/((?!api|_next/static|_next/image|favicon.ico).*)', // Match all routes except some specific ones
 };
 // export const config = {
-//     matcher: '/((?!api|_next/static|_next/image|favicon.ico).*)', // Match all routes except some specific ones
 //   };
